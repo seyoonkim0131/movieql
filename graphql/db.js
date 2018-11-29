0a65b5bbc02cc4f86422395fa3cc49f916caa3e7
Seyoon <rlatpdbs@gmail.com>
2018-11-29 18:56:02 +0900

    11까지

---
+++ graphql/db.js
@@ -0,0 +1,8 @@
+const fetch = require('node-fetch')
+const API_URL = "https://yts.am/api/v2/list_movies.json"
+
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`)
+        .then(res => res.json())
+        .then(json => json.data.movies)
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import {GraphQLServer} from 'graphql-yoga'
-
-const server = new GraphQLServer({})
+import resolvers from './graphql/resolvers'
+const server = new GraphQLServer({
+    typeDefs: 'graphql/schema.graphql',
+    resolvers
+})
 server.start(() => console.log("Graphql Server Running"))
