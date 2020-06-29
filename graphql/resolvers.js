5493417b38d64e3e5aaadf7407866e6d52dd7f3c
Seyoon Kim <seyoon@MacBook-Pro.local>
2020-06-29 21:57:48 +0900

    update schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,15 @@
-import { getMovies, getById, addMovie, deleteMovie } from './db'
+import { getMovies, getMovie, getSuggestions } from './db';
 
 const resolvers = {
     Query: {
-        movies: (_, {rating, limit}) => getMovies(limit, rating)
-    }
-}
+        movies: (_, { rating, limit }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id)
+    },
+    // Mutation: {
+    //     addMovie: (_, { name, score }) => addMovie(name, score),
+    //     deleteMovie: (_, { id }) => deleteMovie(id)
+    // }
+};
 
-export default resolvers
+export default resolvers;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import {GraphQLServer} from 'graphql-yoga'
-import resolvers from './graphql/resolvers'
+import { GraphQLServer  } from 'graphql-yoga';
+import resolvers from './graphql/resolvers';
 const server = new GraphQLServer({
     typeDefs: 'graphql/schema.graphql',
     resolvers
-})
-server.start(() => console.log("Graphql Server Running"))
+});
+server.start(() => console.log("Graphql Server Running"));
