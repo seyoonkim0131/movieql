a49c783453c12da505099ada485bbdc41d33f6f2
Seyoon Kim <seyoon@MacBook-Pro.local>
2020-06-30 23:26:54 +0900

    오류 수정

--- graphql/db.js
+++ graphql/db.js
@@ -3,7 +3,7 @@ const BASE_URL = "https://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
-
+const axios = require('axios');
 // export const getMovies = (limit, rating) => {
 //     let REQUEST_URL = API_URL
 //     if(limit > 0) {
